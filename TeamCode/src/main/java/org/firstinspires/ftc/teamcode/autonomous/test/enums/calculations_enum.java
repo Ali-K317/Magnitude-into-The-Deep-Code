6b483cc97f47581ee7f7fdd7344e74466f13a642
Ali-K317 <akassmieh4233262@gmail.com>
2024-10-22 11:02:15 -0500

    Initial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/test/enums/calculations_enum.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.autonomous.test.enums;
+
+public enum calculations_enum {
+    GEAR_RATIO,
+    COUNTS_PER_MOTOR_REV,
+    WHEEL_DIAMETER_INCHES,
+    COUNTS_PER_INCH,
+    WHEEL_CIRCUMFERENCE_INCHES,
+    TURNING_RADIUS,
+    TURNING_CIRCUMFERENCE,
+    INCH_PER_DEGREE,
+    TICK_PER_INCH,
+    TICK_PER_360,
+    TURN_SPEED,
+    DRIVE_SPEED,
+    ROBOTLENGTH,
+    ROBOTWIDTH,
+    ROBOTHEIGHT,
+    HALFRL,
+    HALFRW,
+    HALFRH
+}
