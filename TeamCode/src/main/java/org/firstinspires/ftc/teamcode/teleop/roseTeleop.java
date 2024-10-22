e1b42be9f51e7d4d17729d3979b057bdc5dbdea4
Ali-K317 <akassmieh4233262@gmail.com>
2024-10-22 11:07:37 -0500

    Moved rose's teleop into the teleop file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/roseTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/roseTeleop.java
@@ -1,14 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp()
 public class roseTeleop extends LinearOpMode {
